32e1c5846c8de1a31ba8909721b986c0f54bfb66
Simon McManis <simon.mcmanis@gmail.com>
2020-05-16 22:33:44 +1000

    start page, about, and results page boilerplate

---
+++ README.md
@@ -0,0 +1,21 @@
+## Project Structure
+
+```
+.
+├── data 
+│   └── HAM1000_dataset
+│       └── ...
+├── skin_cancer 
+│   ├── assets
+│   ├── model
+│   └── app.py
+├── ...
+
+```
+| Folder/File | Description | 
+| ----------- | ----------- |
+| data | Contains all datasets  |
+| skin_cancer/assets | CSS, JS, images... | 
+| skin_cancer/model | CNN stuff |
+| skin_cancer/app.py | Manages and run the Dash app |
+
--- skin_cancer/app.py
+++ skin_cancer/app.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 
 # external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -16,72 +17,97 @@ app = dash.Dash(__name__,
                     }
                 ],)
 
-# header = html.Header(
-#     children=[
-#         html.Div(
-#             className='left-header',
-#             children=[html.H1('MelaKNOWma')]
-#             # ['BetterDriver'], className='header-title'
-#         ),
-#         html.Div(
-#             className='right-header',
-#         )
-#     ]
-# )
-
-
-# start_tab = html.Div(
-#     id='start-content',
-#     className='content',
-#     children=[
-#         html.Div([
-#             dcc.Upload(
-#                 html.Button('Add Photo', id='upload-btn'))
-#         ], className="row"),
-
-#         html.Div(
-#             id='image-row',
-#             children=[
-#                 html.H3("hello"),
-#                 html.Div([
-#                     html.Img(src='assets/example.jpg')
-#                 ])
-#             ]
-#         ),
-#         html.
-#     ]
-# )
 
 start_tab = html.Div(
     id='start-content',
     className='content',
+    style={'display':'none'},
     children=[
         dcc.Upload(
-            html.Button('Add Photo', id='upload')),
+            html.Button('Add Photo', id='upload-btn'), id='upload',
+            accept='image/*', 
+            className='btn',
+            className_active='upload-active',), 
         html.Div(
-            id='image-row',
+            id='img-row',
             children=[
-                html.H3("hello"),
-                html.Div([], id='img-container')
+                html.Div([html.Img(id='display-img', className='display-img')], className='img-container')
             ]
         ),
-        html.Button('Analyze')
+        html.Button('Analyze',id='submit-btn', className='btn disabled')
     ]
 )
 
 
+# loading circle in the img-container whilst upload loaing
+#  button greyed out then green when successful image 
 @app.callback(
-    Output('img-container', 'children'),
-    [Input('upload', 'contents')]
-)
-def upload_image(image):
-    return image
-pass
+    [Output('display-img', 'src'),
+    Output('img-store', 'src')],
+    [Input('upload', 'contents'),
+     Input('upload-btn', 'n_clicks')],
+     [State('img-store', 'src')])
+def upload_img(new_img, n_clicks, store):
+    # if new_img is None:  
+    if new_img is None:
+        if store is None:
+            return '', '' 
+        else:
+            return store, store
+    else:
+        return new_img, new_img
 
-about_tab = html.Div()
+@app.callback(
+    [Output('submit-btn', 'disabled'),
+    Output('submit-btn', 'className')],
+    [Input('display-img', 'src')])
+def set_submit_btn(src):
+    if src != '':
+        return False, 'btn submit-rdy'
+    return True,'btn disabled'
+    
+
+about_tab = html.Div(
+    id='about-content',
+    className='content',
+    style={'display':'none'},
+    children=[
+        html.H1('Skin Cancer App')
+    ]
+)
+
+results_tab = html.Div(
+    id='results-content',
+    className='content',
+    style={'display':'none'},
+    children=[
+        html.Div(
+        children=[
+            html.Div([html.Img(id='analyzed-img', className='display-img')], className='img-container'),
+            html.Div([
+
+            ])
+        ]
+    )]
+)
+
+
+# Make results. might want to store them in a hidden div as well.
+@app.callback(
+    Output('analyzed-img', 'src'),
+    [Input('results-content', 'style')],
+    [State('img-store', 'src')])
+def results(results, src):
+    if src == '':
+        raise PreventUpdate
+
+    # Create image to show with results...
+    analyzed_img = src
+    return analyzed_img
 
 app.layout = html.Div([
     html.Div([
+        html.Img(id='img-store', style={'display': 'none'}),
         dcc.Tabs([
             dcc.Tab(label='About', value='about-tab', className='tab'),
             dcc.Tab(label='Start', value='start-tab', className='tab'),
@@ -89,29 +115,50 @@ app.layout = html.Div([
         ], id='tabs',
             value='start-tab',
             className='tab'),
-        html.Div(id='tabs-container')
+        html.Div(id='tabs-container',
+                 children=[
+                     html.Div([
+                         about_tab,
+                         start_tab,
+                         results_tab
+                     ])
+                 ]),
+
     ], className='container')
 ], className='page')
 
 
-
 @app.callback(
-    Output('tabs-container', 'children'),
+    [Output('about-content', 'style'),
+    Output('start-content', 'style'),
+    Output('results-content', 'style')],
     [Input('tabs', 'value')])
 def render_content(tab):
-    if tab == 'start-tab':
-        return start_tab
-    elif tab == 'about-tab':
-        return about_tab
+    if tab == 'about-tab':
+        return {}, {'display': 'none'}, {'display': 'none'}
+    elif tab == 'start-tab':
+        return {'display': 'none'}, {}, {'display': 'none'}
+    elif tab == 'results-tab':
+        return {'display': 'none'}, {'display': 'none'}, {}
+
+
+# @app.callback(
+#     Output('tabs-container', 'children'),
+#     [Input('tabs', 'value')])
+# def render_content(tab):
+#     if tab == 'start-tab':
+#         return start_tab
+#     elif tab == 'about-tab':
+#         return about_tab
 # @app.callback(
 #     Output("output", "children"),
-#     [Input("upload-image", "value")],
+#     [Input("upload-img", "value")],
 # )
 # def update_selection_mode(img):
 #     if img:
 #         return html.Div([
 #             html.Div(
-#                 id="image",
+#                 id="img",
 #                 children=[
 #                     html.Img(
 #                         src='assets/example.jpg',
